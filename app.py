cf8265fbe42a685091aeabaf4fccfb8de235e3fb
John Lozano <john.lozano@ambonare.com>
2016-11-07 15:55:48 -0600

    fix

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ hostname = socket.gethostname()
 app = Flask(__name__)
 
 def get_redis():
-    if not hasattr(g, 'redis'):
-        g.redis = Redis(host="redis", db=0, socket_timeout=5)
+    if not hasattr(g, 'drRedis'):
+        g.redis = Redis(host="drRedis", db=0, socket_timeout=5)
     return g.redis
 
 @app.route("/", methods=['POST','GET'])
