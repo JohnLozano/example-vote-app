83f4c272d8cf346fcb0b96ccd29a7e13aa904eaa
John Lozano <jlozano2389@gmail.com>
2018-01-10 16:32:41 -0600

    Dogs

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import json
 
 option_a = os.getenv('OPTION_A', "Cats")
 option_b = os.getenv('OPTION_B', "Dogs")
-# option_b = os.getenv('OPTION_B', "Hamsters")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
